8a48faf3b762b77232e1884862760d447b1d7211
Katsuki <Katsuki_Sato0130@icloud.com>
2019-01-21 20:25:21 +0900

    Add One-to-Many

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190121110208) do
+ActiveRecord::Schema.define(version: 20190121112222) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "profiles", force: :cascade do |t|
     t.string "name"
