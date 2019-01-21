a810b5850767998f2c8a70547edb161949c994f0
Katsuki <Katsuki_Sato0130@icloud.com>
2019-01-21 20:22:16 +0900

    Add One-to-One

--- db/migrate/20190121110208_create_profiles.rb
+++ db/migrate/20190121110208_create_profiles.rb
@@ -1,7 +1,8 @@
 class CreateProfiles < ActiveRecord::Migration[5.1]
   def change
     create_table :profiles do |t|
-
+      t.string :name
+      t.references :user
       t.timestamps
     end
   end
